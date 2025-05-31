b4de29aa24758ddc797bc54d82131f692279004a
Harshitha P S <nnm23is076@nmamit.in>
2025-05-31 14:54:22 +0530

    Fix Vite entry path for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'frontend',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@monaco-editor/react'],
   },
   build: {
     target: 'esnext',
+    outDir: 'dist',
   },
   server: {
     headers: {
